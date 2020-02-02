ae489ab610f4a54efbe4e785ad8a836acdcc49c7
bdewhirst <b.dewhirst@gmail.com>
2020-02-02 13:30:29 -0500

    Black 120 and misc. cleanup

--- read_and_save.py
+++ read_and_save.py
@@ -3,9 +3,9 @@ import urllib.request
 from gtts import gTTS
 from bs4 import BeautifulSoup
 
+
 def reader(
-    a_file,
-    a_url,
+    a_file, a_url,
 ):
     """
     inputs:
@@ -17,12 +17,8 @@ def reader(
         raw_page = the_url.read()
 
     parsed_html = BeautifulSoup(raw_page, features="html.parser")
-    title = parsed_html.head.find('title').text  # for testing purposes, only convert the title.
+    title = parsed_html.head.find("title").text  # for testing purposes, only convert the title.
 
-    tts = gTTS(text=title, lang='en')
+    tts = gTTS(text=title, lang="en")
     print("calling Google's api and saving result-- this is the slow part.", a_file)
     tts.save(a_file)
-
-
-if __name__ == "__main__":
-    pass  # don't call this script directly, use main.py instead.
